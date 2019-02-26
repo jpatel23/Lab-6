c531a5b5e4e430ab2bfd9d7f538d3401eb09bacb
Jaimin Patel <jaiminabpatel@ou.edu>
2019-02-26 14:40:45 -0600

    Finished shape class

--- src/Shape.java
+++ src/Shape.java
@@ -96,6 +96,23 @@ public abstract class Shape implements Comparable<Shape>
 	public int compareTo(Shape s)
 	{
 		// TODO: complete this...
+		if (this.getArea() > s.getArea()) {
+			return 1;
+		}
+		else if (this.getArea() < s.getArea()) {
+			return -1;
+		}
+		else {
+			if (this.getPerimeter() > s.getPerimeter()) {
+				return 1;
+			}
+			else if (this.getPerimeter() < s.getPerimeter()) {
+				return -1;
+			}
+			else {
+				return 0;
+			}
+		}
 	}
 
 	/**
@@ -130,5 +147,6 @@ public abstract class Shape implements Comparable<Shape>
 	public String toString()
 	{
 		// TODO: complete this...
+		g
 	}
 }
